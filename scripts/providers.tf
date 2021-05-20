e9e68cf75eb7905bb6f67546d093a1d4bafc4b27
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-05-20 20:10:39 +0800

    Format

--- scripts/providers.tf
+++ scripts/providers.tf
@@ -3,8 +3,8 @@
 provider "azurerm" {
   features {}
 
-  tenant_id       = "${var.tenant_id}"
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.agent_client_id}"
-  client_secret   = "${var.agent_client_secret}"
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.agent_client_id
+  client_secret   = var.agent_client_secret
 }
